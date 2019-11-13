f31b37d6bf88230e8d86135c2f32473a6883f889
Byron Dominguez <byrond27@gmail.com>
2019-11-13 12:07:57 +0100

    delete return variables in City class

--- src/City.php
+++ src/City.php
@@ -8,16 +8,14 @@ class City {
   const ARRAY_NUMBER = 0;
 
   public function getWoeid ($client, $city) {
-      $woeid = json_decode($client->get(SELF::URL.'search/?query='.$city)->getBody()->getContents(),
+      return json_decode($client->get(SELF::URL.'search/?query='.$city)->getBody()->getContents(),
       true)[SELF::ARRAY_NUMBER]['woeid'];
-      return $woeid;
   }
 
   public function findPrediction($client,$woeid) {
       // Find the predictions for the city
-      $results = json_decode($client->get(SELF::URL.$woeid)->getBody()->getContents(),
-      true)['consolidated_weather'];
-      return $results;    
+      return json_decode($client->get(SELF::URL.$woeid)->getBody()->getContents(),
+      true)['consolidated_weather']; 
   }
 
 }
