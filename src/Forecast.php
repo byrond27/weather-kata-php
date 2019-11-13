4c6c48a7487feeee25ca1024303b82fcbcda525f
Byron Dominguez <byrond27@gmail.com>
2019-11-13 11:07:04 +0100

    class Woeid

--- src/Forecast.php
+++ src/Forecast.php
@@ -4,11 +4,10 @@ namespace Codium\CleanCode;
 
 use GuzzleHttp\Client;
 
-const NUMBER = 0;
-const API_URL = 'https://www.metaweather.com/api/location/';
-
 class Forecast
 {
+    CONST DATE_PREDICTION = "+6 days 00:00:00";
+    
     public function predict(string &$city, \DateTime $datetime = null, bool $wind = false): string
     {
         // When date is not provided we look for the current prediction
@@ -17,56 +16,48 @@ class Forecast
         }
 
         // If there are predictions
-        if ($datetime >= new \DateTime("+6 days 00:00:00")) {
+        if ($datetime >= new \DateTime(self::DATE_PREDICTION)) {
             return "";
         }
-
         // Create a Guzzle Http Client
         $client = new Client();
-        $newCity = new City($client,$city);
 
         // Find the id of the city on metawheather
-        $city = $newCity->getWoeid();;
-
+        $mycity = new Woeid();
+        $city = $mycity->getWoeid($client,$city);
         // Find the predictions for the city
-        $results = $newCity->getPredictionResults();
+        $results = $mycity->findPrediction($client,$city);
+
         foreach ($results as $result) {
             // When the date is the expected
             if ($result["applicable_date"] == $datetime->format('Y-m-d')) {
                 // If we have to return the wind information
-                return ($wind) ? $result['wind_speed']: $result['weather_state_name'];
+                if ($wind) {
+                    return $result['wind_speed'];
+                } else {
+                    return $result['weather_state_name'];
+                }
             }
         }
     }
 }
 
-class City{
-    private $woeid;
-    private $results;
-    public $name;
+class Woeid {
 
-    function __construct($client,$city) {
-        $this->name = $city;
-        $this->setWoeid($client,$city);
-        $this->setPredictionResults($client,$this->getWoeid());
+    const URL = 'https://www.metaweather.com/api/location/';
+
+    public function getWoeid ($client, $city) {
+        
+        $woeid = json_decode($client->get(SELF::URL.'search/?query='.$city)->getBody()->getContents(),
+        true)[0]['woeid'];
+        return $woeid;
     }
 
-    function setWoeid($client,$city){
-        $this->woeid = json_decode($client->get(API_URL.'search/?query='.$city)->getBody()->getContents(),
-        true)[NUMBER]['woeid'];
-    }
-
-    function getWoeid(){
-        return $this->woeid;
-    }
-
-    function setPredictionResults($client,$woeid){
-        $this->results = json_decode($client->get(API_URL.$woeid)->getBody()->getContents(),
-            true)['consolidated_weather'];
-    }
-
-    function getPredictionResults(){
-        return $this->results;
+    public function findPrediction($client,$woeid) {
+    // Find the predictions for the city
+    $results = json_decode($client->get(SELF::URL.$woeid)->getBody()->getContents(),
+    true)['consolidated_weather'];
+    return $results;
     }
 
 }
