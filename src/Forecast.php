04798dc560aa8b0bba169520d302d7749d258ab2
Byron Dominguez <byrond27@gmail.com>
2019-11-13 12:02:12 +0100

    split in two files

--- src/Forecast.php
+++ src/Forecast.php
@@ -38,22 +38,3 @@ class Forecast
         }
     }
 }
-class City {
-  
-    const URL = 'https://www.metaweather.com/api/location/';
-    const ARRAY_NUMBER = 0;
-
-    public function getWoeid ($client, $city) {
-        $woeid = json_decode($client->get(SELF::URL.'search/?query='.$city)->getBody()->getContents(),
-        true)[SELF::ARRAY_NUMBER]['woeid'];
-        return $woeid;
-    }
-
-    public function findPrediction($client,$woeid) {
-        // Find the predictions for the city
-        $results = json_decode($client->get(SELF::URL.$woeid)->getBody()->getContents(),
-        true)['consolidated_weather'];
-        return $results;    
-    }
-
-}
